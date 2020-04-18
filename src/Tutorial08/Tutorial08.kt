56ecfbc0b7b9c8dbcd7ebeb4b2252e9200c6583a
martinyounghoonkim <hoons0131@gmail.com>
2020-04-18 23:27:31 +0900

    const val 키워드

---
+++ src/Tutorial08/Tutorial08.kt
@@ -0,0 +1,11 @@
+package Tutorial08
+
+// 최상위 프로퍼티에서만 const 키워드를 사용할 수 있음
+// 자바의 public static final 과 동일
+const val UNIX_LINE_SEPARATOR = "\n"
+
+fun main() {
+
+}
+
+fun String.lastChar (): Char = this.get(this.length - 1)
